46a4b3bf52846bc99c5377fa0c09e7839292b2d6
paulokataoka <katchchupp@gmail.com>
2025-05-13 16:13:06 -0300

    Update agendar.js

--- agendar.js
+++ agendar.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     );
 
     // Adicionando os horários da noite
-    const horariosDisponiveis = ['09:00', '11:00', '13:00', '15:00', '17:00', '19:00', '20:30'];
+    const horariosDisponiveis = ['19:00', '20:30'];
 
     const horaSelect = document.getElementById('hora');
     horariosDisponiveis.forEach((hora) => {
